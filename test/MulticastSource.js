4a352175718a50afb6b5d8c75fbb99d53aa07db4
Brian Cavalier <brian@hovercraftstudios.com>
2016-02-29 20:21:30 -0500

    Add eslint-config-standard, tweak. Remove jscs

--- test/MulticastSource.js
+++ test/MulticastSource.js
@@ -1,30 +1,30 @@
-import {describe, it} from 'mocha';
-import assert from 'assert';
-import sinon from 'sinon';
+import {describe, it} from 'mocha'
+import assert from 'assert'
+import sinon from 'sinon'
 
-import {of, reduce, drain, Stream} from 'most';
-import scheduler from 'most/lib/scheduler/defaultScheduler';
+import {of, reduce, drain, Stream} from 'most'
+import scheduler from 'most/lib/scheduler/defaultScheduler'
 
-import MulticastSource from '../src/MulticastSource';
-import FakeDisposeSource from './helper/FakeDisposeSource';
+import MulticastSource from '../src/MulticastSource'
+import FakeDisposeSource from './helper/FakeDisposeSource'
 
-const sentinel = {value: 'sentinel'};
-const other = {value: 'other'};
+const sentinel = {value: 'sentinel'}
+const other = {value: 'other'}
 
-describe('MulticastSource' , () => {
+describe('MulticastSource', () => {
   it('should call producer on first subscriber', () => {
-    const eventSpy = sinon.spy();
-    const s = new MulticastSource({run: () => {}});
+    const eventSpy = sinon.spy()
+    const s = new MulticastSource({run: () => {}})
 
-    s.run({event: eventSpy}, scheduler);
-    s.event(sentinel);
+    s.run({event: eventSpy}, scheduler)
+    s.event(sentinel)
 
-    assert.strictEqual(eventSpy.calledOnce, true);
-  });
+    assert.strictEqual(eventSpy.calledOnce, true)
+  })
 
   it('should call producer ONLY on the first subscriber', () => {
-    const sourceSpy = sinon.spy();
-    const s = new MulticastSource({run: sourceSpy});
+    const sourceSpy = sinon.spy()
+    const s = new MulticastSource({run: sourceSpy})
 
     Promise.all([
       s.run({event: () => {}}, scheduler),
@@ -32,27 +32,27 @@ describe('MulticastSource' , () => {
       s.run({event: () => {}}, scheduler),
       s.run({event: () => {}}, scheduler)
     ]).then(() => {
-      assert.strictEqual(sourceSpy.calledOnce, true);
-    });
-  });
+      assert.strictEqual(sourceSpy.calledOnce, true)
+    })
+  })
 
   it('should publish events to all subscribers', () => {
-    const s = new MulticastSource(of(sentinel).source);
-    const second = (_, y) => y;
+    const s = new MulticastSource(of(sentinel).source)
+    const second = (_, y) => y
 
     Promise.all([reduce(second, other, s), reduce(second, other, s)])
-      .then(values => {
-        assert.strictEqual(values[0], sentinel)/
-        assert.strictEqual(values[1], sentinel);
-      });
-  });
+      .then((values) => {
+        assert.strictEqual(values[0], sentinel) /
+        assert.strictEqual(values[1], sentinel)
+      })
+  })
 
   it('should call dispose if all subscribers disconnect', () => {
-    const spy = sinon.spy();
-    const s = new Stream(new MulticastSource(FakeDisposeSource.from(spy, of(sentinel))));
+    const spy = sinon.spy()
+    const s = new Stream(new MulticastSource(FakeDisposeSource.from(spy, of(sentinel))))
 
     Promise.all([drain(s), drain(s)]).then(() => {
-      assert.strictEqual(spy.calledOnce, true);
-    });
-  });
-});
+      assert.strictEqual(spy.calledOnce, true)
+    })
+  })
+})
--- test/helper/FakeDisposeSource.js
+++ test/helper/FakeDisposeSource.js
@@ -1,16 +1,16 @@
 import dispose from 'most/lib/disposable/dispose'
 
 export default class FakeDisposeSource {
-  constructor(disposer, source) {
+  constructor (disposer, source) {
     this.source = source
     this.disposable = dispose.create(disposer)
   }
 
-  run(sink, scheduler) {
+  run (sink, scheduler) {
     return dispose.all([this.source.run(sink, scheduler), this.disposable])
   }
 
-  static from(disposer, stream) {
+  static from (disposer, stream) {
     return new FakeDisposeSource(disposer, stream.source)
   }
 }
