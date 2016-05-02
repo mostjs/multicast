163f8044e77c4fdf258b8f7cbd91751073a87300
Brian Cavalier <brian@hovercraftstudios.com>
2016-05-02 07:33:39 -0400

    Dispose only once to avoid race condition

--- src/MulticastDisposable.js
+++ src/MulticastDisposable.js
@@ -2,11 +2,15 @@ export default class MulticastDisposable {
   constructor (source, sink) {
     this.source = source
     this.sink = sink
+    this.disposed = false
   }
 
   dispose () {
-    const s = this.source
-    const remaining = s.remove(this.sink)
-    return remaining === 0 && s._dispose()
+    if (this.disposed) {
+      return
+    }
+    this.disposed = true
+    const remaining = this.source.remove(this.sink)
+    return remaining === 0 && this.source._dispose()
   }
 }
