e2dfb761febc45e2638893e5233b64fd12348ec4
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-05-31 07:31:26 -0400

    chore(TS): add type definitions (#17)
    * chore(TS): add type definitions
    
    * chore(TS): fix resolution issues

---
+++ type-definitions/index.d.ts
@@ -0,0 +1,18 @@
+import {
+  Stream,
+  Sink,
+  Scheduler,
+  Source,
+  Disposable
+} from "most";
+
+export default function multicast<A>(s: Stream<A>): Stream<A>;
+
+export class MulticastSource<A> implements Source<A>, Sink<A> {
+  run (sink: Sink<A>, scheduler: Scheduler): Disposable<A>;
+  add(sink: Sink<A>): number;
+  remove(sink: Sink<A>): number;
+  event(time: number, value: A): void;
+  end(time: number, value: A): void;
+  error(time: number, err: Error): void;
+}
