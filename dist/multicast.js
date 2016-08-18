d68b7bb6a62690c070ff0ea052833f3fc4b7e938
Tylor Steinberger <tlsteinberger167@gmail.com>
2016-08-17 21:09:38 -0400

    Rollup buble buba (#20)
    * chore(babelrc): remove babelrc
    
    No longer use babel
    
    * chore(rollup): add rollup configuration
    
    * chore(rollup): fix rollup configuration
    
    Fix name of output file
    
    * chore(package): switch to rollup, buble, and buba
    
    Use rollup + buble for build-dist
    Use istanbul + buba for unit-test
    Add jsnext:main field for next-gen package bundlers
    Add src/ to files field to be uploaded to npm
    
    * chore(package): add missing rollup dev dep
    
    * chore(rollup): fix moduleName
    
    Change moduleName from most -> mostMulticast
    
    * chore(rollup): fix global name for @most/prelude
    
    * chore(dist): build dist/
    
    * chore(package): update all dependencies
    
    * chore(package): add most as a dev dependency

--- dist/multicast.js
+++ dist/multicast.js
@@ -1,174 +1,116 @@
 (function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define('@most/multicast', ['exports', '@most/prelude'], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports, require('@most/prelude'));
-  } else {
-    var mod = {
-      exports: {}
-    };
-    factory(mod.exports, global.prelude);
-    global.mostMulticast = mod.exports;
-  }
-})(this, function (exports, _prelude) {
-  'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('@most/prelude')) :
+  typeof define === 'function' && define.amd ? define(['exports', '@most/prelude'], factory) :
+  (factory((global.mostMulticast = global.mostMulticast || {}),global.mostPrelude));
+}(this, function (exports,_most_prelude) { 'use strict';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-  exports.MulticastSource = undefined;
-
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  var _createClass = function () {
-    function defineProperties(target, props) {
-      for (var i = 0; i < props.length; i++) {
-        var descriptor = props[i];
-        descriptor.enumerable = descriptor.enumerable || false;
-        descriptor.configurable = true;
-        if ("value" in descriptor) descriptor.writable = true;
-        Object.defineProperty(target, descriptor.key, descriptor);
-      }
-    }
-
-    return function (Constructor, protoProps, staticProps) {
-      if (protoProps) defineProperties(Constructor.prototype, protoProps);
-      if (staticProps) defineProperties(Constructor, staticProps);
-      return Constructor;
-    };
-  }();
-
-  var MulticastDisposable = function () {
-    function MulticastDisposable(source, sink) {
-      _classCallCheck(this, MulticastDisposable);
-
-      this.source = source;
-      this.sink = sink;
-      this.disposed = false;
-    }
-
-    _createClass(MulticastDisposable, [{
-      key: 'dispose',
-      value: function dispose() {
-        if (this.disposed) {
-          return;
-        }
-        this.disposed = true;
-        var remaining = this.source.remove(this.sink);
-        return remaining === 0 && this.source._dispose();
-      }
-    }]);
-
-    return MulticastDisposable;
-  }();
-
-  function tryEvent(t, x, sink) {
-    try {
-      sink.event(t, x);
-    } catch (e) {
-      sink.error(t, e);
-    }
-  }
-
-  function tryEnd(t, x, sink) {
-    try {
-      sink.end(t, x);
-    } catch (e) {
-      sink.error(t, e);
-    }
-  }
-
-  var dispose = function dispose(disposable) {
-    return disposable.dispose();
+  var MulticastDisposable = function MulticastDisposable (source, sink) {
+    this.source = source
+    this.sink = sink
+    this.disposed = false
   };
+
+  MulticastDisposable.prototype.dispose = function dispose () {
+    if (this.disposed) {
+      return
+    }
+    this.disposed = true
+    var remaining = this.source.remove(this.sink)
+    return remaining === 0 && this.source._dispose()
+  };
+
+  function tryEvent (t, x, sink) {
+    try {
+      sink.event(t, x)
+    } catch (e) {
+      sink.error(t, e)
+    }
+  }
+
+  function tryEnd (t, x, sink) {
+    try {
+      sink.end(t, x)
+    } catch (e) {
+      sink.error(t, e)
+    }
+  }
+
+  var dispose = function (disposable) { return disposable.dispose(); }
 
   var emptyDisposable = {
-    dispose: function dispose() {}
-  };
-
-  var MulticastSource = function () {
-    function MulticastSource(source) {
-      _classCallCheck(this, MulticastSource);
-
-      this.source = source;
-      this.sinks = [];
-      this._disposable = emptyDisposable;
-    }
-
-    _createClass(MulticastSource, [{
-      key: 'run',
-      value: function run(sink, scheduler) {
-        var n = this.add(sink);
-        if (n === 1) {
-          this._disposable = this.source.run(this, scheduler);
-        }
-        return new MulticastDisposable(this, sink);
-      }
-    }, {
-      key: '_dispose',
-      value: function _dispose() {
-        var disposable = this._disposable;
-        this._disposable = emptyDisposable;
-        return Promise.resolve(disposable).then(dispose);
-      }
-    }, {
-      key: 'add',
-      value: function add(sink) {
-        this.sinks = (0, _prelude.append)(sink, this.sinks);
-        return this.sinks.length;
-      }
-    }, {
-      key: 'remove',
-      value: function remove(sink) {
-        var i = (0, _prelude.findIndex)(sink, this.sinks);
-        // istanbul ignore next
-        if (i >= 0) {
-          this.sinks = (0, _prelude.remove)(i, this.sinks);
-        }
-
-        return this.sinks.length;
-      }
-    }, {
-      key: 'event',
-      value: function event(time, value) {
-        var s = this.sinks;
-        if (s.length === 1) {
-          return s[0].event(time, value);
-        }
-        for (var i = 0; i < s.length; ++i) {
-          tryEvent(time, value, s[i]);
-        }
-      }
-    }, {
-      key: 'end',
-      value: function end(time, value) {
-        var s = this.sinks;
-        for (var i = 0; i < s.length; ++i) {
-          tryEnd(time, value, s[i]);
-        }
-      }
-    }, {
-      key: 'error',
-      value: function error(time, err) {
-        var s = this.sinks;
-        for (var i = 0; i < s.length; ++i) {
-          s[i].error(time, err);
-        }
-      }
-    }]);
-
-    return MulticastSource;
-  }();
-
-  function multicast(stream) {
-    var source = stream.source;
-    return source instanceof MulticastSource ? stream : new stream.constructor(new MulticastSource(source));
+    dispose: function dispose$1 () {}
   }
 
+  var MulticastSource = function MulticastSource (source) {
+    this.source = source
+    this.sinks = []
+    this._disposable = emptyDisposable
+  };
+
+  MulticastSource.prototype.run = function run (sink, scheduler) {
+    var n = this.add(sink)
+    if (n === 1) {
+      this._disposable = this.source.run(this, scheduler)
+    }
+    return new MulticastDisposable(this, sink)
+  };
+
+  MulticastSource.prototype._dispose = function _dispose () {
+    var disposable = this._disposable
+    this._disposable = emptyDisposable
+    return Promise.resolve(disposable).then(dispose)
+  };
+
+  MulticastSource.prototype.add = function add (sink) {
+    this.sinks = _most_prelude.append(sink, this.sinks)
+    return this.sinks.length
+  };
+
+  MulticastSource.prototype.remove = function remove$1 (sink) {
+    var i = _most_prelude.findIndex(sink, this.sinks)
+    // istanbul ignore next
+    if (i >= 0) {
+      this.sinks = _most_prelude.remove(i, this.sinks)
+    }
+
+    return this.sinks.length
+  };
+
+  MulticastSource.prototype.event = function event (time, value) {
+    var s = this.sinks
+    if (s.length === 1) {
+      return s[0].event(time, value)
+    }
+    for (var i = 0; i < s.length; ++i) {
+      tryEvent(time, value, s[i])
+    }
+  };
+
+  MulticastSource.prototype.end = function end (time, value) {
+    var s = this.sinks
+    for (var i = 0; i < s.length; ++i) {
+      tryEnd(time, value, s[i])
+    }
+  };
+
+  MulticastSource.prototype.error = function error (time, err) {
+    var s = this.sinks
+    for (var i = 0; i < s.length; ++i) {
+      s[i].error(time, err)
+    }
+  };
+
+  function multicast (stream) {
+    var source = stream.source
+    return source instanceof MulticastSource
+      ? stream
+      : new stream.constructor(new MulticastSource(source))
+  }
+
+  exports['default'] = multicast;
   exports.MulticastSource = MulticastSource;
-  exports.default = multicast;
-});
+
+  Object.defineProperty(exports, '__esModule', { value: true });
+
+}));
+//# sourceMappingURL=multicast.js.map
---
+++ rollup.config.js
@@ -0,0 +1,15 @@
+import buble from 'rollup-plugin-buble'
+
+export default {
+  entry: 'src/index.js',
+  dest: 'dist/multicast.js',
+  format: 'umd',
+  moduleName: 'mostMulticast',
+  sourceMap: true,
+  plugins: [
+    buble()
+  ],
+  globals: {
+    '@most/prelude': 'mostPrelude'
+  }
+}
